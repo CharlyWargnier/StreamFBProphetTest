78576ba59fb5d8b63e10df24d1d813ad16c28ec5
Charly Wargnier <cwar05@gmail.com>
2020-10-23 23:13:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import streamlit as st
 
-
-
-st.header('aaa')
-st.write('aaa')
+st.header('Test')
+st.write('Test')
 
 import pandas as pd
 from fbprophet import Prophet
 
-#df = pd.read_csv('/content/example_wp_log_peyton_manning.csv')
 df = pd.read_csv('example_wp_log_peyton_manning.csv')
 
 st.write(df.head())
 
+m = Prophet()
+m.fit(df)
 
+future = m.make_future_dataframe(periods=365)
+future.tail()
 
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
+fig1 = m.plot(forecast)
 
-
-
-
+st.write(fig1)
 
 
 
