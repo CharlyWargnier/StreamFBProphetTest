ff8740c952d1f4dfaeb0f04e0656a659be29f142
Charly Wargnier <cwar05@gmail.com>
2020-10-24 00:16:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ st.write('Test')
 import pandas as pd
 from fbprophet import Prophet
 
-df = pd.read_csv('example_wp_log_peyton_manning.csv')
+df = pd.read_csv('/content/example_wp_log_peyton_manning.csv')
+
+#/content/example_wp_log_peyton_manning.csv
 
 st.write(df.head())
 
